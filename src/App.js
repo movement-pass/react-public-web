b0a76ca3fd67385d0067be7ca49bcd6f47549e56
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-08-30 03:04:34 +0600

    refactor: updated banner link

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ function App() {
                   can find the complete implementation on the following link -
                   <strong>
                     <MuiLink
-                      href="https://github.com/movement-pass"
+                      href="https://github.com/movement-pass/movement-pass.github.io"
                       color="inherit"
                     >
                       https://github.com/movement-pass
